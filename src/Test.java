a8c062be60a2e514704d301143038add30059d8f
KIMKYEONGYONG <86136741+KIMKYEONGYONG@users.noreply.github.com>
2021-06-19 12:05:47 +0900

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,5 @@ public class Test {
   
   public static void main(String[] args){
     System.out.println("안녕하세요"); 
-    
-    for(int i=0; i< 10; i++;){
-      System.out.println(i+"안녕하세요"); 
-    }
   }
 }
